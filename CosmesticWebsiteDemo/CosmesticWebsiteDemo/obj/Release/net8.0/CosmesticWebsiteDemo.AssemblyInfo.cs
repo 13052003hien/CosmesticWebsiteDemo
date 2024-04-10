7219600ce7cfc8b1fef7cc4e9392120b89e0b3ca
HienTran <hienxadoi2020@gmail.com>
2024-04-10 14:21:42 +0700

    commit

--- CosmesticWebsiteDemo/CosmesticWebsiteDemo/obj/Release/net8.0/CosmesticWebsiteDemo.AssemblyInfo.cs
+++ CosmesticWebsiteDemo/CosmesticWebsiteDemo/obj/Release/net8.0/CosmesticWebsiteDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-CosmesticWebsiteDemo-c787b757-5b6d-4d55-82ea-c92621270c9f")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CosmesticWebsiteDemo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f90f95430ebc0f1c658e01685a902d408d1437f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CosmesticWebsiteDemo")]
